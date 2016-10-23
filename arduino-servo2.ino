21bf6888f6bc285e9eeddb4397851470e3a17a84
ryu <ryutarou-freeks@msn.com>
2016-10-23 22:17:53 +0900

    Added codes for servo2 that control 360 degree motor.

--- arduino-servo2.ino
+++ arduino-servo2.ino
@@ -1,24 +1,23 @@
-#include <Servo.h> 
-Servo servo;        //Servoオブジェクトを作成
+#include <Servo.h>
+Servo servo1;        //Servo1オブジェクトを作成
+Servo servo2;        //Servo2オブジェクトを作成
 int BTReset = 3;
-int LED = 13;
 
 char inByte = 0;
 int nowAction = 0;
 
 boolean isServoPosiInit = false;
 void setup() {
-  servo.attach(9);  //D9ピンをサーボの信号線として設定
+  servo1.attach(9);  //D9ピンをサーボ1の信号線として設定
+  servo2.attach(8);  //D8ピンをサーボ2の信号線として設定
   // make the Bluetooth Module reset:
   digitalWrite(BTReset, LOW);
   delay(100);
   digitalWrite(BTReset, HIGH);
   delay(500);
-   
+
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
-
-  pinMode(LED, OUTPUT);  // make a LED pin output
 }
 
 void loop() {
@@ -28,21 +27,45 @@ void loop() {
     inByte = Serial.read();
     if (inByte == '0') {
       nowAction = 0;
+    } else if (inByte == '1') {
+      nowAction = 1;
+    } else if (inByte == '2') {
+      nowAction = 2;
+    } else if (inByte == '3') {
+      nowAction = 3;
+    } else if (inByte == '4') {
+      nowAction = 4;
     } else if (inByte == '5') {
       nowAction = 5;
-    } else {
+    } else if (inByte == '6') {
+      nowAction = 6;
+    } else if (inByte == '9') {
       nowAction = 9;
+    } else {
+      nowAction - 1;
     }
-    Serial.println(inByte); 
+    Serial.println(inByte);
     delay(100);
   }
   delay(1);        // delay in between reads for stability
 
   if (nowAction == 0) {
-      servo.write(0); // サーボの角度を0°に設定
-      digitalWrite(LED, LOW);
+    servo1.write(0); // サーボ1の角度を0°に設定
+  } else if (nowAction == 1) {
+    servo2.write(0); // サーボ2の角度を0°に設定
+  } else if (nowAction == 2) {
+    servo2.write(9); // サーボ2の角度を60°に設定
+  } else if (nowAction == 3) {
+    servo2.write(21); // サーボ2の角度を120°に設定
+  } else if (nowAction == 4) {
+    servo2.write(35); // サーボ2の角度を180°に設定
+  } else if (nowAction == 5) {
+    servo2.write(49); // サーボ2の角度を240°に設定
+  } else if (nowAction == 6) {
+    servo2.write(63); // サーボ2の角度を300°に設定
+  } else if (nowAction == 9) {
+    servo1.write(90); // サーボ1の角度を90°に設定
   } else {
-      servo.write(77); // サーボの角度を90°に設定
-      digitalWrite(LED, HIGH);
+    // do nothing
   }
 }
